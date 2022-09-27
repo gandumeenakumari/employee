09f89e7af083d6aecc4b42f62524d5ec9a99fedc
meena kumari gandu <meenakumarigandu@gmail.com>
2022-09-27 12:22:32 +0530

    employee

---
+++ fullstack/src/app/Model/Employee.Model.ts
@@ -0,0 +1,9 @@
+export interface Employee
+{
+    id:string;
+    name:String;
+    email:string;
+    phone:number;
+    salary:number;
+    department:string;
+}
---
+++ src/app/components/Employees/add-employee/add-employee.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from 'src/app/Model/Employee.Model';
+import { EmployeesService } from 'src/app/Services/employees.service';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent implements OnInit {
+
+  addEmployeeRequest:Employee={
+    id:'',
+    name:'',
+    email:'',
+    phone:'',
+    salary:0,
+    department:''
+
+  };
+  constructor(private route:ActivatedRoute,private employeesService:EmployeesService,private router:Router) {
+   }
+
+  ngOnInit(): void {
+  }
+  addEmployee()
+  {
+    console.log("Add employee method triggred");
+
+    this.employeesService.addEmployee(this.addEmployeeRequest)
+    .subscribe({
+      next:(Employees) =>{
+        console.log(Employees);
+        this.router.navigate(['Employees']);
+      }
+    });
+  }
+}
